1dae8e74b38591a827c3538cb7035e3143c3e140
joaa-hash <12395500+joaa-hash@users.noreply.github.com>
2024-08-17 15:06:12 -0600

    meetings_controller: submit meting data to zoho

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,7 @@
 class MeetingsController < ApplicationController
+  include HTTParty
+  base_uri "https://www.zohoapis.com/crm/v2"
+
   def index
     response = { message: "Hello, World!" }
     render json: response
@@ -12,7 +15,6 @@ class MeetingsController < ApplicationController
       start_time = params[:start_time]
       end_time = params[:end_time]
       summary = params[:summary]
-      action_items = params[:action_items]
       report_url = params[:report_url]
       puts "Session ID: #{session_id}"
       puts "Title: #{title}"
@@ -21,10 +23,40 @@ class MeetingsController < ApplicationController
       puts "Summary: #{summary}"
       puts "Report URL: #{report_url}"
 
-      render json: { message: "Meeting created successfully" }
+      submit_to_zoho_crm(params)
     else
-    response = { message: "We did not receive meeting data" }
-    render json: response
+      response = { message: "We did not receive meeting data" }
+      render json: response
+    end
+  end
+
+  private
+
+  def submit_to_zoho_crm(params)
+    # Prepare the request options
+    options = {
+      headers: {
+        "Authorization" => "Zoho-oauthtoken #{ENV['ZOHO_ACCESS_TOKEN']}",
+        "Content-Type" => "application/json"
+      },
+      body: {
+        session_id: params[:session_id],
+        summary: params[:summary],
+        action_items: params[:action_items],
+        report_url: params[:report_url]
+      }.to_json
+    }
+    # Sending the POST request
+    response = self.class.post("/meetings", options)
+    puts response
+
+    # Handling the response
+    if response.success?
+      puts "Meeting created successfully in Zoho CRM"
+      render json: { message: "Meeting created successfully in Zoho CRM", zoho_response: response.parsed_response }
+    else
+      puts "Failed to create meeting in Zoho CRM"
+      render json: { message: "Failed to create meeting in Zoho CRM", error: response.body }, status: :bad_request
     end
   end
 end
