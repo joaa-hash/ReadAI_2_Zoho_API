ecc8b5ca371ff564598c93ee967bc6c33b22bef5
joaa-hash <12395500+joaa-hash@users.noreply.github.com>
2024-08-16 21:35:11 -0600

    MeetingsController: add action new

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,4 +3,16 @@ class MeetingsController < ApplicationController
     response = { message: "Hello, World!" }
     render json: response
   end
+
+  def new
+    if params[:session_id].present?
+      # Send data in params to ZOHO API
+      # we response succesfully
+      pust params
+      render json: { message: "Meeting created successfully" }
+    else
+    response = { message: "We did not receive meeting data" }
+    render json: response
+    end
+  end
 end
