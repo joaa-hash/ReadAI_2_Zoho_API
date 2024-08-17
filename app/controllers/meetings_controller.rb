7dbf238d06262e442ff474cc6bed69af1840a970
joaa-hash <12395500+joaa-hash@users.noreply.github.com>
2024-08-17 14:19:33 -0600

    meetings controller: extract sesiong_id, summary, action times, report url, title, from params

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -6,9 +6,22 @@ class MeetingsController < ApplicationController
 
   def new
     if params[:session_id].present?
-      # Send data in params to ZOHO API
-      # we response succesfully
-      puts params
+      # we get session_id, sumary, action_items, report_url, start_time, end_time and title
+      session_id = params[:session_id]
+      title = params[:title]
+      start_time = params[:start_time]
+      end_time = params[:end_time]
+      summary = params[:summary]
+      action_items = params[:action_items]
+      report_url = params[:report_url]
+      puts "Session ID: #{session_id}"
+      puts "Title: #{title}"
+      puts "Start Time: #{start_time}"
+      puts "End Time: #{end_time}"
+      puts "Summary: #{summary}"
+      puts "Action Items: #{action_items}"
+      puts "Report URL: #{report_url}"
+
       render json: { message: "Meeting created successfully" }
     else
     response = { message: "We did not receive meeting data" }
