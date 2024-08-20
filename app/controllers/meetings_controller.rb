1238f51beb7341de1ee0fcf0c828575f374673b1
joaa-hash <12395500+joaa-hash@users.noreply.github.com>
2024-08-20 17:41:29 -0600

    meetingController: removes puts

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -16,20 +16,6 @@ class MeetingsController < ApplicationController
 
   def new
     if params[:session_id].present?
-      # we get session_id, sumary, report_url, start_time, end_time and title
-      session_id = params[:session_id]
-      title = params[:title]
-      start_time = params[:start_time]
-      end_time = params[:end_time]
-      summary = params[:summary]
-      report_url = params[:report_url]
-      puts "Session ID: #{session_id}"
-      puts "Title: #{title}"
-      puts "Start Time: #{start_time}"
-      puts "End Time: #{end_time}"
-      puts "Summary: #{summary}"
-      puts "Report URL: #{report_url}"
-
       if @access_token.nil? || Time.now > @expires_in
         refresh_access_token
       end
